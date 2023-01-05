e63bd16a737d2e04de2ed316b6ad72c2624db2cc
withydn <withydn@naver.com>
2023-01-05 21:37:32 +0900

    0105
    0105

--- a-diary/src/pages/start.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function start() {
-  return <div>start</div>;
-}
---
+++ a-diary/src/pages/start/start.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styled from './start';
+
+export default function start() {
+  return <div class={styled.container}>start</div>;
+}
